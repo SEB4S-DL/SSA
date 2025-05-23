361acf0c9ed69fab2989acb133f7e6b4c6b591d9
Sebas Duque <duquelopezsebastian796@gmail.com>
2025-05-23 11:47:44 -0500

    En proceso

--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,11 @@
-<?php
-session_start();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
