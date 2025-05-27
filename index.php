11f664797566bdaf2940f6dfa83e674864361271
Sebas Duque <duquelopezsebastian796@gmail.com>
2025-05-27 07:29:43 -0500

    Delete unnecesary files

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./assets/img/sena-logo.png">
     <link rel="stylesheet" href="./assets/css/global.css">
-    <?php include_once "./includes/bootstrap.php"; ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body onload="activarModoOscuro('global.css', 'bootstrap-icons')">
     <main>
