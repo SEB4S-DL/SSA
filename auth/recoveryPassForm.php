9ff961fbf61d6ea6c7bb90ce096fa571a727b48b
S3B4S-DL <duquelopezsebastian796@gmail.com>
2025-09-22 11:58:17 -0500

    Formulario de recuperacion de contraseña

---
+++ auth/recoveryPassForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/recoveryPassForm.css">
+  <title>Recuperación</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Recuperación de Contraseña</h1>
+    <form action="">
+      <label for="idInput">Ingrese la nueva contraseña</label>
+      <input type="password" id="idInput" placeholder="Ingrese la nueva contraseña" required>
+
+      <label for="idInput2">Confirme la contraseña</label>
+      <input type="password" id="idInput2" placeholder="Confirme la nueva contraseña" required>
+
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
+</body>
+</html>
